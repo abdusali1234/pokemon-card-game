844ff6ecd2a27f7d988579c767b46e8c25921c15
abdusali1234 <abdusali1234@gmail.com>
2025-06-07 07:50:00 +0100

    added shuffle feature and successfully shuffled list

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,26 @@
 <script>
   import Card from './lib/Card.svelte';
   import { pokemons } from './lib/pokemonData';
-  
+
+  function shuffle(arr) {
+    let shuffled = arr
+      .map((val) => ({ val, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ val }) => val);
+    return shuffled;
+  }
+  let pokemonData = $state(pokemons);
+
 </script>
 
 <main>
   <ul>
-    {#each pokemons as pokemon}
+    {#each pokemonData as pokemon}
       <li>{pokemon.id}, {pokemon.name}</li>
       
       
     {/each}
+    <button onclick={() => pokemonData=  shuffle(pokemonData)}>Shuffle the list</button>
   </ul>
   
 </main>
