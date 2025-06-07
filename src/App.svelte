008e5e96f29c27d71085970224793c669d4b19cb
abdusali1234 <abdusali1234@gmail.com>
2025-06-07 08:00:32 +0100

    added dialog element and ssytling. Need to add functionality so that it opens and closes.

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,13 @@
   import Card from './lib/Card.svelte';
   import { pokemons } from './lib/pokemonData';
 
+  
+  let pokemonData = $state(pokemons);
+  let selectedPokemonIds = $state([]);
+  let currScore = $state(0);
+  let bestScore = $state(0);
+  let modalDialogMessage = $state("");
+
   function shuffle(arr) {
     let shuffled = arr
       .map((val) => ({ val, sort: Math.random() }))
@@ -9,11 +16,17 @@
       .map(({ val }) => val);
     return shuffled;
   }
-  let pokemonData = $state(pokemons);
+  
 
 </script>
 
 <main>
+  <dialog id="game-over-dialog">
+        <div>
+          <h1></h1>
+          <button>Reset</button>
+        </div>
+      </dialog>
   <ul>
     {#each pokemonData as pokemon}
       <li>{pokemon.id}, {pokemon.name}</li>
@@ -26,5 +39,24 @@
 </main>
 
 <style>
+  #game-over-dialog {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    -ms-transform: translate(-50%, -50%);
+    -moz-transform: translate(-50%, -50%);
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    border-radius: 1em;
+    background-color: var(--bg-colour-1);
+    box-shadow: var(--shadow-elevation-high);
+  }
+  
+  #game-over-dialog > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
  
 </style>
